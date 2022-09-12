922bd1c1f26693af3f9a99585d986d0f5da7ea69
rodrigobenta <rodrigobentacorg@gmail.com>
2022-09-12 13:18:51 -0300

    Fixed?

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-<<<<<<< HEAD:api/router/userRoutes.js
 const router = express.Router();
 const {listUsers, listUserById, createUser, editUserById, deleteUserById, login} = require('../controller/userController');
 const verify = require('../middleware/verifyJWT');
@@ -17,15 +16,5 @@ router.post('/login', login);
 //RUTAS DE CARTS:
 router.get('/:id/cart', verify, listUserById);
 router.put('/:id/cart', verify, editUserById);
-=======
-const {login, show, create, edit, deleteU} = require('../controllers/userController');
-const verify = require('../middleware/verifyJWT');
-const router = express.Router();
-
-router.post('/login', login);
-router.post('/create', verify, create);
-router.put('/edit/:id', verify, edit);
-router.delete('/delete/:id', verify, deleteU);
->>>>>>> 06c080cde587a73d19c8a3289870aa9e50c71912:api/routes/userRoutes.js
 
 module.exports = router;
